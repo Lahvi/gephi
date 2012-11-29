b496faf665ab29650fcc6f00ddb5606871a2398f
Lahvi_2 <Lahvi_2@147.32.78.115>
2012-11-29 11:00:55 +0100

    "jen komenty"

--- DHNSGraph/src/org/gephi/graph/dhns/graph/HierarchicalGraphImpl.java
+++ DHNSGraph/src/org/gephi/graph/dhns/graph/HierarchicalGraphImpl.java
@@ -1,43 +1,43 @@
 /*
-Copyright 2008-2010 Gephi
-Authors : Mathieu Bastian <mathieu.bastian@gephi.org>
-Website : http://www.gephi.org
+ Copyright 2008-2010 Gephi
+ Authors : Mathieu Bastian <mathieu.bastian@gephi.org>
+ Website : http://www.gephi.org
 
-This file is part of Gephi.
+ This file is part of Gephi.
 
-DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
 
-Copyright 2011 Gephi Consortium. All rights reserved.
+ Copyright 2011 Gephi Consortium. All rights reserved.
 
-The contents of this file are subject to the terms of either the GNU
-General Public License Version 3 only ("GPL") or the Common
-Development and Distribution License("CDDL") (collectively, the
-"License"). You may not use this file except in compliance with the
-License. You can obtain a copy of the License at
-http://gephi.org/about/legal/license-notice/
-or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
-specific language governing permissions and limitations under the
-License.  When distributing the software, include this License Header
-Notice in each file and include the License files at
-/cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
-License Header, with the fields enclosed by brackets [] replaced by
-your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
+ The contents of this file are subject to the terms of either the GNU
+ General Public License Version 3 only ("GPL") or the Common
+ Development and Distribution License("CDDL") (collectively, the
+ "License"). You may not use this file except in compliance with the
+ License. You can obtain a copy of the License at
+ http://gephi.org/about/legal/license-notice/
+ or /cddl-1.0.txt and /gpl-3.0.txt. See the License for the
+ specific language governing permissions and limitations under the
+ License.  When distributing the software, include this License Header
+ Notice in each file and include the License files at
+ /cddl-1.0.txt and /gpl-3.0.txt. If applicable, add the following below the
+ License Header, with the fields enclosed by brackets [] replaced by
+ your own identifying information:
+ "Portions Copyrighted [year] [name of copyright owner]"
 
-If you wish your version of this file to be governed by only the CDDL
-or only the GPL Version 3, indicate your decision by adding
-"[Contributor] elects to include this software in this distribution
-under the [CDDL or GPL Version 3] license." If you do not indicate a
-single choice of license, a recipient has the option to distribute
-your version of this file under either the CDDL, the GPL Version 3 or
-to extend the choice of license to its licensees as provided above.
-However, if you add GPL Version 3 code and therefore, elected the GPL
-Version 3 license, then the option applies only if the new code is
-made subject to such option by the copyright holder.
+ If you wish your version of this file to be governed by only the CDDL
+ or only the GPL Version 3, indicate your decision by adding
+ "[Contributor] elects to include this software in this distribution
+ under the [CDDL or GPL Version 3] license." If you do not indicate a
+ single choice of license, a recipient has the option to distribute
+ your version of this file under either the CDDL, the GPL Version 3 or
+ to extend the choice of license to its licensees as provided above.
+ However, if you add GPL Version 3 code and therefore, elected the GPL
+ Version 3 license, then the option applies only if the new code is
+ made subject to such option by the copyright holder.
 
-Contributor(s):
+ Contributor(s):
 
-Portions Copyrighted 2011 Gephi Consortium.
+ Portions Copyrighted 2011 Gephi Consortium.
  */
 package org.gephi.graph.dhns.graph;
 
@@ -67,11 +67,10 @@ import org.gephi.graph.dhns.utils.TreeNodeWrapper;
 public abstract class HierarchicalGraphImpl extends AbstractGraphImpl implements HierarchicalGraph {
 
     protected final Predicate<AbstractNode> enabledNodePredicate;
-    
+
     public HierarchicalGraphImpl(Dhns dhns, GraphViewImpl view) {
         super(dhns, view);
         enabledNodePredicate = new Predicate<AbstractNode>() {
-
             public boolean evaluate(AbstractNode element) {
                 return element.isEnabled();
             }
@@ -203,16 +202,16 @@ public abstract class HierarchicalGraphImpl extends AbstractGraphImpl implements
 
     public Node getOpposite(Node node, Edge edge) {
         checkNode(node);
-        AbstractEdge absEdge = checkEdgeOrMetaEdge(edge);
+//        AbstractEdge absEdge = checkEdgeOrMetaEdge(edge);
 //        if (absEdge.getSource(view.getViewId()) == node) {
 //            return absEdge.getTarget(view.getViewId());
 //        } else if (absEdge.getTarget(view.getViewId()) == node) {
 //            return absEdge.getSource(view.getViewId());
 //        }
         //TODO: Pouze docasne reseni, chce to vyresit proc je po filtrovani jine viewID u grafu a u jeho prvku (node edge)
-        if(edge.getSource().equals(node)){
+        if (edge.getSource().equals(node)) {
             return edge.getTarget();
-        } else if(edge.getTarget().equals(node)) {
+        } else if (edge.getTarget().equals(node)) {
             return edge.getSource();
         }
         throw new IllegalArgumentException("Node must be either source or target of the edge.");
